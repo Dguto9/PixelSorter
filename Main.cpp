c66bede9f708c9e7ce8c04d6c93c124b60e854a0
Dillon Gutowski <dillongutowski@gmail.com>
2023-10-21 15:05:40 -0400

    Add hysteresis thresholding code

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <math.h>
 
+void recursiveThreshold(Matrix* edges, int column, int row, int reachX, int reachY);
+
 int main() {
 	Image image("C:/Users/dillo/OneDrive/Documents/Projects/MatrixModule/PixelSorter/Images/ethan.bmp");
 	Matrix imageGrey = (image.pixels[0]+image.pixels[1]+image.pixels[2]) * (1.0f/3.0f);
@@ -38,9 +40,7 @@ int main() {
 	nms = magnitudes;
 	
 	for (int i = 0; i < image.height; i++) {
-		//std::cout << "LMAO THE THINGY WAHOO" << std::endl;
 		for (int j = 0; j < image.width; j++) {
-			//std::cout << "LETS GO GOLFING" << std::endl;
 			if (i == 0 || i == (image.height - 1) || j == 0 || j == (image.width - 1)) {
 				nms.data[j + (i * image.width)] = 0.0;
 				continue;
@@ -72,26 +72,49 @@ int main() {
 			else {
 				std::cout << angles.data[j + (i * image.width)] / imgProcUtils::pi << std::endl;
 			}
-			//std::cout << "I can hardly elieve me eyes" << std::endl;
 		}
 	}
 	float lowThreshold = 0.07;
-	float highThreshold = 0.2;
+	float highThreshold = 0.17;
 	for (int i = 0; i < image.height * image.width; i++) {
-		nms.data[i] = nms.data[i] < lowThreshold ? 0 : (nms.data[i] > highThreshold ? 1 : nms.data[i]);
+		nms.data[i] = nms.data[i] < lowThreshold ? 0 : (nms.data[i] > highThreshold ? 1 : 0.5);
+	}
+
+	Matrix hysteresis = nms;
+	for (int i = 0; i < image.height; i++) {
+		for (int j = 0; j < image.width; j++) {
+			recursiveThreshold(&hysteresis, j, i, 1, 1);
+		}
 	}
 
 	for (int i = 0; i < image.height * image.width; i++) {
-		nms.data[i] = imgProcUtils::clamp(nms.data[i], 0, 1);
 		magnitudes.data[i] = imgProcUtils::clamp(magnitudes.data[i], 0, 1);
+		hysteresis.data[i] = hysteresis.data[i] < 1 ? 0 : 1;
 	}
 
+	Image hyst = hysteresis;
 	Image nonmax = nms;
 	Image magn = magnitudes;
 	Image anglR = anglesRound;
+	hyst.saveToBMP("C:/Users/dillo/OneDrive/Documents/Projects/MatrixModule/PixelSorter/Images/Output/ethanHysteresis.bmp");
 	nonmax.saveToBMP("C:/Users/dillo/OneDrive/Documents/Projects/MatrixModule/PixelSorter/Images/Output/ethanNMS.bmp");
 	magn.saveToBMP("C:/Users/dillo/OneDrive/Documents/Projects/MatrixModule/PixelSorter/Images/Output/ethanMagnitudes.bmp");
 	anglR.saveToBMP("C:/Users/dillo/OneDrive/Documents/Projects/MatrixModule/PixelSorter/Images/Output/ethanAngles.bmp");
 	return 0;
 }
 
+void recursiveThreshold(Matrix* edges, int column, int row, int reachX, int reachY) {
+	if (edges->data[column + (row * edges->columns)] == 1) {
+		for (int i = -reachY; i <= reachY; i++) {
+			for (int j = -reachX; j <= reachX; j++) {
+				if (column + j >= 0 && column + j < edges->columns && row + i >= 0 && row + i < edges->rows) {
+					if (edges->data[(column + j) + ((row + i) * edges->columns)] == 0.5) {
+						edges->data[(column + j) + ((row + i) * edges->columns)] = 1;
+						recursiveThreshold(edges, column + j, row + i, reachX, reachY);
+					}
+				}
+			}
+		}
+	}
+	return;
+}
